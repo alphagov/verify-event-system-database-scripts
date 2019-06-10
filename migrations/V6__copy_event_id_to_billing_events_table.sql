1686661c285beed853bc50e8e1c287826a472747
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2019-06-10 17:48:53 +0100

    Bgg1HWGD: Populate new event_id column in billing_events table
    Fix invalid SQL causing test failures.

--- migrations/V6__copy_event_id_to_billing_events_table.sql
+++ migrations/V6__copy_event_id_to_billing_events_table.sql
@@ -1,5 +1,5 @@
 UPDATE billing.billing_events be
-   SET be.event_id = ae.event_id
+   SET event_id = ae.event_id
   FROM audit.audit_events ae
  WHERE be.event_id IS NULL
    AND ae.session_id = be.session_id
