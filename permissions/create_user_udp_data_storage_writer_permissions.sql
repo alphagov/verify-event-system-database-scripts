1930a15e3054de404cdf3f4408ff637c53dcfb4e
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2019-09-27 18:24:35 +0100

    YYdbmOJJ: Lambda to process the data that's uploaded and put it into the database
    Update permissions for `idp_data_storage_writer`

--- permissions/create_user_udp_data_storage_writer_permissions.sql
+++ permissions/create_user_udp_data_storage_writer_permissions.sql
@@ -2,5 +2,6 @@ GRANT CONNECT ON DATABASE events TO idp_data_storage_writer;
 
 GRANT USAGE ON SCHEMA idp_data TO idp_data_storage_writer;
 GRANT USAGE ON SCHEMA billing TO idp_data_storage_writer;
-GRANT SELECT, INSERT, DELETE ON idp_data.* TO idp_data_storage_writer;
+GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA idp_data TO idp_data_storage_writer;
+GRANT USAGE ON ALL SEQUENCES IN SCHEMA idp_data TO idp_data_storage_writer;
 GRANT SELECT ON billing.fraud_events TO idp_data_storage_writer;
