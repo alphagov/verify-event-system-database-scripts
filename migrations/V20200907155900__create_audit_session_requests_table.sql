1392d23d19e7bccb8d9f579ff39cdc660680394a
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2020-09-09 11:53:30 +0100

    HUB-717: create new audit session request table
    Creating a new table to hold unique session_id and request_id combination

---
+++ migrations/V20200907155900__create_audit_session_requests_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE audit.audit_event_session_requests
+(
+    session_id           text COLLATE pg_catalog."default" NOT NULL,
+    request_id           text COLLATE pg_catalog."default" NOT NULL
+)
